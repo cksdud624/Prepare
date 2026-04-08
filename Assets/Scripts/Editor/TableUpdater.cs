ccf2b558b85b844aee6ff25372db93dcadb8a0b0
cyhuh <cksdud624@naver.com>
2026-04-08 20:58:50 +0900

    Table Generator 개선

--- Assets/Scripts/Editor/TableUpdater.cs
+++ Assets/Scripts/Editor/TableUpdater.cs
@@ -1,89 +1,144 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Security.Cryptography;
+using System.Text;
 using UnityEditor;
 using UnityEngine;
+using System.Linq;
 
 public static class TableUpdater
 {
-    private const string BytesDirectory = "Assets/Tables/";
+    private const string BytesDirectory = "Generated/Table";
+    private const string ScriptsDirectory = "Scripts/Generated/Table";
+    private const string TableDirectory = ".Table/";
+    private const string TableManifestFile = "TableManifest.json";
     [MenuItem("Table/Update Table")]
     public static void UpdateTable()
     {
-        string[] guids = AssetDatabase.FindAssets("t:TextAsset", new[] {BytesDirectory});
-        HashSet<string> bytesHash = new HashSet<string>();
-        foreach (string guid in guids)
-        {
-            string path = AssetDatabase.GUIDToAssetPath(guid); 
-            path = path.Replace(BytesDirectory, "");
-            path = path.Replace(".bytes", "");
-            bytesHash.Add(path);
-        }
+        string tableDirectoryPath = Path.Combine(Application.dataPath, TableDirectory);
+        string tableManifestFilePath = Path.Combine(tableDirectoryPath, TableManifestFile);
         
-        string directory = Path.Combine(Application.dataPath, ".Tables");
-        if (!Directory.Exists(directory))
-        {
-            Debug.Log("Table Directory Not Found");
-            return;
-        }
-        string bytesDirectoryPath = Path.Combine(Application.dataPath, "Tables");
-        string[] tsvPaths = Directory.GetFiles(directory, "*.tsv", SearchOption.TopDirectoryOnly);
+        TableManifest tableManifest;
+        if (File.Exists(tableManifestFilePath))
+            tableManifest = JsonUtility.FromJson<TableManifest>(File.ReadAllText(tableManifestFilePath));
+        else
+            tableManifest = new TableManifest();
         
-        foreach (string tsvPath in tsvPaths)
+        if (!Directory.Exists(tableDirectoryPath))
+            Directory.CreateDirectory(tableDirectoryPath);
+        
+        string[] tablePaths = Directory.GetFiles(tableDirectoryPath, "*.tsv", SearchOption.TopDirectoryOnly);
+        
+        TableManifest newTableManifest = new TableManifest();
+        Dictionary<TableHashData, List<string>> scriptReference = new();
+        foreach (var tablePath in tablePaths)
         {
-            string tableName = Path.GetFileNameWithoutExtension(tsvPath);
-            if (bytesHash.Contains(tableName))
-                bytesHash.Remove(tableName);
+            string[] lines = File.ReadAllLines(tablePath);
+            string tableName = Path.GetFileNameWithoutExtension(tablePath);
             
-            DateTime tableLastWriteTime = File.GetLastWriteTime(tsvPath);
-            string bytesPath = Path.Combine(bytesDirectoryPath, tableName + ".bytes");
-            bool hasBytes = File.Exists(bytesPath);
-            if (hasBytes)
+            if (lines.Length < 2)
             {
-                DateTime bytesLastWriteTime = File.GetLastWriteTime(bytesPath);
-                if (tableLastWriteTime > bytesLastWriteTime)
-                    CreateBytes(tsvPath, bytesPath);
+                Debug.Log($"{tablePath} has 2 less lines");
+                return;
+            }
+
+            byte[] tableDataBytes;
+            using (MemoryStream ms = new MemoryStream())
+            using (BinaryWriter bw = new BinaryWriter(ms))
+            {
+                foreach (var line in lines)
+                    bw.Write(line);
+                tableDataBytes = ms.ToArray();
+            }
+
+            string md5Hash = string.Empty;
+            using (MD5 md5 = MD5.Create())
+            {
+                byte[] hashBytes = md5.ComputeHash(tableDataBytes);
+                StringBuilder sb = new StringBuilder();
+                foreach (var b in hashBytes)
+                    sb.Append(b.ToString("x2"));
+                md5Hash = sb.ToString();
+            }
+
+            TableHashData hashData = tableManifest.tables.FirstOrDefault(data => data.fileName == tableName);
+            if (hashData != null)
+            {
+                if (hashData.hash != md5Hash)
+                {
+                    CreateTable(tableName ,tableDataBytes);
+                    TableHashData newHashData = new TableHashData(tableName, md5Hash);
+                    newTableManifest.tables.Add(newHashData);
+                    scriptReference.Add(newHashData, new List<string>{lines[0], lines[1]});
+                }
+                else
+                {
+                    newTableManifest.tables.Add(hashData);
+                    scriptReference.Add(hashData, new List<string>{lines[0], lines[1]});
+                }
+                tableManifest.tables.Remove(hashData);
             }
             else
-                CreateBytes(tsvPath, bytesPath);
+            {
+                CreateTable(tableName, tableDataBytes);
+                TableHashData newHashData = new TableHashData(tableName, md5Hash);
+                newTableManifest.tables.Add(newHashData);
+                scriptReference.Add(newHashData, new List<string>{lines[0], lines[1]});
+            }
         }
 
-        foreach (var bytesPath in bytesHash)
+        if (tableManifest.tables.Count > 0)
         {
-            AssetDatabase.DeleteAsset(BytesDirectory + bytesPath + ".bytes");
-            Debug.Log($"<color=#ADD8E6>[Table Delete Success]</color> {bytesPath}.bytes");
+            foreach (var tableName in tableManifest.tables)
+            {
+                string tablePath = Path.Combine(Application.dataPath, BytesDirectory, tableName.fileName) + ".bytes";
+                if (File.Exists(tablePath))
+                    File.Delete(tablePath);
+            }
         }
+        
+        //scriptReference
+        
+        /*
+         * 1. TableManifest.json을 로드
+         * 
+         * 2. .Table의 모든 테이블들을 모두 가져온다
+         * 3. .Table을 bytes파일로 변환한다.
+         * 4. TableManifest.json에 있는 json 정보들과 .Table의 모든 tsv와 비교한다
+         * 5. TableManifest.json에 없는 테이블들은 추가하고 변경되었으면 변경하고 TableManifest.json에 주회후 데이터가 남아있으면 bytes파일을 삭제한다
+         * 6. 수정, 추가, 삭제된 bytes파일에 맞게 스크립트 디렉토리도 수정, 추가, 삭제한다
+         */
+        AssetDatabase.Refresh();
     }
 
-    private static void CreateBytes(string tsvPath, string bytesPath)
+    private static void CreateTable(string tableName, byte[] byteDatas)
     {
-        try
-        {
-            string[] lines = File.ReadAllLines(tsvPath);
-            long lastWriteTicks = File.GetLastWriteTime(tsvPath).Ticks;
+        string bytesDirectoryPath = Path.Combine(Application.dataPath, BytesDirectory);
+        if(!Directory.Exists(bytesDirectoryPath))
+            Directory.CreateDirectory(bytesDirectoryPath);
+        string bytesPath = Path.Combine(bytesDirectoryPath, tableName + ".bytes");
+        
+        File.WriteAllBytes(bytesPath, byteDatas);
+    }
+    
 
-            string directory = Path.GetDirectoryName(bytesPath);
-            if (!Directory.Exists(directory)) Directory.CreateDirectory(directory);
-            using (FileStream fs = new FileStream(bytesPath, FileMode.Create))
-            {
-                using (BinaryWriter writer = new BinaryWriter(fs))
-                {
-                    writer.Write(lastWriteTicks);
-                    int dataCount = Mathf.Max(0, lines.Length - 1);
-                    writer.Write(dataCount);
-                    for (int i = 1; i < lines.Length; i++)
-                    {
-                        writer.Write(lines[i]);
-                    }
-                }
-            }
+    [Serializable]
+    public class TableManifest
+    {
+        public List<TableHashData> tables = new List<TableHashData>();
+    }
 
-            Debug.Log($"<color=#ADD8E6>[Table Create Success]</color> {Path.GetFileName(tsvPath)} -> {Path.GetFileName(bytesPath)}");
-        }
-        catch (Exception e)
+    [Serializable]
+    public class TableHashData
+    {
+        public string fileName;
+        public string hash;
+
+        public TableHashData(string filename, string hash)
         {
-            Debug.LogError($"[Bake Error] {tsvPath} 변환 실패: {e.Message}");
+            this.fileName = filename;
+            this.hash = hash;
         }
-        AssetDatabase.Refresh();
     }
 }
